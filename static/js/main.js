f7b21072b040f083ed5e3ce63e975d000815d8b0
Boki136 <boris.gersic1@gmail.com>
2021-08-01 01:33:11 +0000

    [refactor rating]: implement rating logic in the template to achieve same thing with less code.

--- static/js/main.js
+++ static/js/main.js
@@ -22,11 +22,15 @@ $(document).ready(function () {
 
     $(".navbar-toggler").click(function () {
         $('.mobile_nav').fadeIn(200);
+        $('.nav-content-wrapper').css('display', 'none');
+        $('.navbar').css('height', '0')
     })
 
     // Close Mobile Menu
     $('.fa-times').click(function () {
         $('.mobile_nav').fadeOut(200);
+        $('.nav-content-wrapper').show();
+        $('.navbar').css('height', '110px')
     })
 
 
@@ -109,79 +113,6 @@ $(document).ready(function () {
         let image_url = $(this).attr('src');
         $('.product-images img:eq(0)').attr('src', `${image_url}`)
     })
-
-    // Calculate product rating & show correct number of stars
-
-    let rating_score = $('.rating-score').text()
-
-    function calculateStars(num) {
-
-        // if score is zero, display message no ratings
-        if (num == 0) {
-            $('.product-rating').replaceWith(`<h4>No Ratings</h4>`)
-        }
-        //checking 0.1 - 0.99
-        else if (num > 0 && num < 0.99) {
-            let empty_stars = $('.product-rating i').slice(1, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-            $('.product-rating i:eq(0)').removeClass('fa-star')
-            $('.product-rating i:eq(0)').addClass('fa-star-half-alt')
-        }
-        //checking 1 - 1.99
-        else if (num == 1) {
-            $('.product-rating i').slice(1, 5)
-            let empty_stars = $('.product-rating i').slice(1, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-        } else if (num > 1 && num < 1.99) {
-            let empty_stars = $('.product-rating i').slice(2, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-            $('.product-rating i:eq(1)').removeClass('fa-star')
-            $('.product-rating i:eq(1)').addClass('fa-star-half-alt')
-            //checking 2 - 2.99
-        } else if (num == 2) {
-            $('.product-rating i:eq(1)').removeClass('fa-star-half-alt')
-            $('.product-rating i:eq(1)').addClass('fa-star')
-            let empty_stars = $('.product-rating i').slice(2, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-        } else if (num > 2 && num < 2.99) {
-            let empty_stars = $('.product-rating i').slice(3, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-            $('.product-rating i:eq(2)').removeClass('fa-star')
-            $('.product-rating i:eq(2)').addClass('fa-star-half-alt')
-            //checking 3 - 3.99
-        } else if (num == 3) {
-            $('.product-rating i').slice(3, 5)
-            let empty_stars = $('.product-rating i').slice(3, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-        } else if (num > 3 && num < 3.99) {
-            let empty_stars = $('.product-rating i').slice(3, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-            $('.product-rating i:eq(3)').removeClass('far')
-            $('.product-rating i:eq(3)').addClass('fas fa-star-half-alt')
-            //checking 4 - 4.99
-        } else if (num == 4) {
-            let empty_stars = $('.product-rating i').slice(4, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-            $('.product-rating i:eq(3)').removeClass('fa-star-half-alt')
-            $('.product-rating i:eq(3)').addClass('fa-star')
-        } else if (num > 4 && num < 4.99) {
-            let empty_stars = $('.product-rating i').slice(4, 5);
-            empty_stars.removeClass('fas')
-            empty_stars.addClass('far')
-            $('.product-rating i:eq(4)').removeClass('far')
-            $('.product-rating i:eq(4)').addClass('fas fa-star-half-alt')
-        }
-    }
-
-    calculateStars(rating_score);
 
     // Transition for next button on homepage slider
 
