3cc1ccc5d2fa5237308dd218756fb01a6275651b
Boki136 <boris.gersic1@gmail.com>
2021-08-02 21:07:24 +0000

    feat[bag contents]: implement funcionality to update bag contents with ptoducts & prices.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -6,15 +9,27 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        price = int(product.price) * 0.011
+        total += quantity * price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_TRESHOLD:
-        delivery = total + settings.STANDARD_DELIVERY_COST
+        delivery = settings.STANDARD_DELIVERY_COST
         free_delivery_acquire = settings.FREE_DELIVERY_TRESHOLD - total
     else:
         delivery = 0
         free_delivery_acquire = 0
 
-    grand_total = delivery + total
+    grand_total = Decimal(delivery + total)
 
     context = {
         'bag_items': bag_items,
