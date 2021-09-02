a8df702d9ef37d21f119c42b7be849b81a6b5ab1
Boki136 <boris.gersic1@gmail.com>
2021-09-02 22:39:53 +0000

    fix product size issue

--- footlook/settings.py
+++ footlook/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'footlook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL_HEROKU' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
